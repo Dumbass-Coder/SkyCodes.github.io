50a64b63d44e345a9ec9184aba6ccdead574509b
Dumbass-Coder <115169198+Dumbass-Coder@users.noreply.github.com>
2022-11-23 19:41:28 +0500

    Add files via upload

---
+++ java1.js
@@ -0,0 +1,58 @@
+console.log('This is Java Script of Sky Codes');
+let para = document.getElementById('para');
+// para.style.display='none'
+function toggelHide(){
+    let btn = document.getElementById('btn');
+    let para = document.getElementById('para');
+    if (para.style.display != 'block') {
+        para.style.display = 'block';
+        console.log('Showing more')
+    } else {
+        para.style.display = 'none';
+        console.log('Showing less')
+    }
+}
+function toggelHide1(){
+    let btn = document.getElementById('fb');
+    let para = document.getElementById('fp');
+    if (para.style.display != 'block') {
+        para.style.display = 'block';
+        console.log('Showing more')
+    } else {
+        para.style.display = 'none';
+        console.log('Showing less')
+    }
+}
+function toggelHide2(){
+    let btn = document.getElementById('fb2');
+    let para = document.getElementById('fp2');
+    if (para.style.display != 'block') {
+        para.style.display = 'block';
+        console.log('Showing more')
+    } else {
+        para.style.display = 'none';
+        console.log('Showing less')
+    }
+}
+function toggelHide3(){
+    let btn = document.getElementById('fb3');
+    let para = document.getElementById('fp3');
+    if (para.style.display != 'block') {
+        para.style.display = 'block';
+        console.log('Showing more')
+    } else {
+        para.style.display = 'none';
+        console.log('Showing less')
+    }
+}
+function toggelHide4(){
+    let btn = document.getElementById('fb4');
+    let para = document.getElementById('fp4');
+    if (para.style.display != 'block') {
+        para.style.display = 'block';
+        console.log('Showing more')
+    } else {
+        para.style.display = 'none';
+        console.log('Showing less')
+    }
+}
